64059f4676c094fe69f24b63fb0e703de4d07aa2
yj <yuyeonju1109@gmail.com>
2020-06-03 19:29:55 +0900

    mapper 수정

--- src/main/java/com/team/juseom/dao/mybatis/mapper/BidderMapper.java
+++ src/main/java/com/team/juseom/dao/mybatis/mapper/BidderMapper.java
@@ -1,13 +1,27 @@
 package com.team.juseom.dao.mybatis.mapper;
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
 import com.team.juseom.domain.Bidder;
 
 public interface BidderMapper {
-	//mapper xml에서 정의함
+
+	@Select("SELECT BIDDERID AS rateId, " 
+			+ "USERID AS userId, "
+			+ "AUCTIONID AS auctionId, " 
+			+ "BIDNUMBER AS bidNumber"
+			+ "BIDPRICE AS bidPrice"
+			+ "FROM RATE "
+			+ "WHERE RATEDID = #{userId}"
+	)
 	List<Bidder> getBidderListByautionId(String auctionId);
 	
-	//mapper xml에서 정의함
+	
+	@Insert("INSERT INTO BIDDER(BIDDERID, USERID, AUCTIONID, BIDNUMBER, BIDPRICE)" 
+	+ "VALUES (#{bidderId}, #{userId}, #{auctionId}, #{bidNumber}, #{bidPrice})")
 	void insertBidder(Bidder bidder);
 	
 }
--- src/main/java/com/team/juseom/dao/mybatis/mapper/RateMapper.java
+++ src/main/java/com/team/juseom/dao/mybatis/mapper/RateMapper.java
@@ -2,12 +2,33 @@ package com.team.juseom.dao.mybatis.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.team.juseom.domain.Rate;
 
 public interface RateMapper {
 
+	@Select("SELECT RATEID AS rateId, " 
+			+ "RATERID AS raterId, "
+			+ "RATEDID AS ratedId, " 
+			+ "RATE AS rate"
+			+ "DESCRIPTION AS description"
+			+ "FROM RATE "
+			+ "WHERE RATEDID = #{userId}"
+	)
 	List<Rate> getRateListByUser(String userId);
+	
+	
 	void insertRate(Rate rate);
+	
+	@Select("SELECT RATEID as rateId, " 
+			+ "RATERID as raterId, "
+			+ "RATEDID as ratedId, " 
+			+ "RATE as rate"
+			+ "DESCRIPTION as description"
+			+ "FROM RATE "
+			+ "WHERE RATEID = #{rateId}"
+	)
 	Rate getRate(String rateId);
 }
 
