dd5b52f4869f81d291b0d95332c830fe7bab7a06
EunSil <zindala@naver.com>
2020-06-29 18:10:15 +0900

    등록 로그인 인터셉터 위치 변경

--- src/main/java/com/team/juseom/controller/LoginInterceptor.java
+++ src/main/java/com/team/juseom/controller/LoginInterceptor.java
@@ -16,21 +16,11 @@ public class LoginInterceptor implements HandlerInterceptor {
 			throws Exception {
 
 		UserSession userSession = (UserSession)WebUtils.getSessionAttribute(request, "userSession");
-		
-		if (userSession == null) {
+		if (userSession == null || userSession.getUser() == null) {
 			String url = request.getRequestURL().toString(); 
 			String query = request.getQueryString();
 			System.out.println(query);
-			if(url.contains("sale.do")) {
-				url = url.replace("sale.do", "book.do");
-				query="isbn=" + request.getParameter("isbn");
-			}else if(url.contains("auction.do")) {
-				url = url.replace("auction.do", "book.do");
-				query="isbn=" + request.getParameter("isbn");
-			}else if(url.contains("share.do")) {
-				url = url.replace("share.do", "book.do");
-				query="isbn=" + request.getParameter("isbn");
-			}
+			
 			ModelAndView modelAndView = new ModelAndView("loginForm");
 			
 			if (query != null) {
